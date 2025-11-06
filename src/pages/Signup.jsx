054430a3c654011208956acd79dd659f59826237
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-06 21:36:20 +0530

    profile picture component and env variable setup done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,6 +7,7 @@ import axiosConfig from "../utils/axiosConfig";
 import { API_ENDPOINTS } from "../utils/apiEndpoints";
 import toast from "react-hot-toast";
 import { Loader2 } from "lucide-react";
+import ProfilePhotoSelector from "../components/ProfilePhotoSelector";
 
 const Signup = () => {
   const [fullName, setFullName] = useState("");
@@ -14,6 +15,7 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [profilePhoto, setProfilePhoto] = useState(null);
 
   const navigate = useNavigate();
 
@@ -79,6 +81,12 @@ const Signup = () => {
           </p>
 
           <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="flex justify-between mb-6">
+              <ProfilePhotoSelector
+                image={profilePhoto}
+                setImage={setProfilePhoto}
+              />
+            </div>
             <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
               <Input
                 value={fullName}
--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = import.meta.env.VITE_BACKEND_API_URL;
+const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUNDINARY_CLOUD_NAME;
 
 export const API_ENDPOINTS = {
   LOGIN: `${BASE_URL}/login`,
@@ -6,4 +7,5 @@ export const API_ENDPOINTS = {
   status: `${BASE_URL}/status`,
   activate: `${BASE_URL}/activate`,
   health: `${BASE_URL}/health`,
+  UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 };
